1c2344357a03ecba06c9fd5bfc683dfed0a06f09
PedroAlmeida18 <pedrohas35@gmail.com>
2023-06-16 09:48:19 -0300

    Formação em java - curso 02 ( Orientação a objeto)  : Ultilização de pastas para sair da principal, métodfos get e set para permitir acessar variáveis privadas

--- src/Principal.java
+++ src/Principal.java
@@ -1,9 +1,11 @@
+import br.com.alura.ScreenMatch.modelos.Filme;
+
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme(); // Tipo referência
-        meuFilme.nome = " O poderoso Chefão";
-        meuFilme.anoDelançamento = 1978;
-        meuFilme.duraçãoFilme = 180;
+        meuFilme.setNome(" O poderoso Chefão");;
+        meuFilme.setAnoDelançamento(1978);
+        meuFilme.setDuraçãoFilme(180);
         meuFilme.exibeFicha();
         meuFilme.avalia(7);
         meuFilme.avalia(7);
