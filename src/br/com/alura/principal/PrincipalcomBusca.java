96a7fb7141104c4860d12daa4bf87236b3e35c62
PedroAlmeida18 <pedrohas35@gmail.com>
2023-07-27 09:44:09 -0300

    Curso java orientação a objeto formação alura - AULA 01 busca de filmes na api, melhorias com Scanner para diversos filmes

--- src/br/com/alura/principal/PrincipalcomBusca.java
+++ src/br/com/alura/principal/PrincipalcomBusca.java
@@ -5,12 +5,17 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class PrincipalcomBusca {
     public static void main(String[]args)  throws IOException, InterruptedException {
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite o nome do texto que você deseja procurar:");
+        var busca = leitura.nextLine();
+        String end = "http://www.omdbapi.com/?t="+busca+ "&apikey=43dbd051";
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://www.omdbapi.com/?i=tt3896198&apikey=43dbd051"))
+                .uri(URI.create(end))
                 .build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         System.out.println(response.body());
