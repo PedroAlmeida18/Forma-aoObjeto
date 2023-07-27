6f6d547ae2c036b1e47d8f9325a9abbd31d8ca33
PedroAlmeida18 <pedrohas35@gmail.com>
2023-07-27 09:07:08 -0300

    Curso java orientação a objeto formação alura - AULA 01

--- src/br/com/alura/principal/PrincipalcomBusca.java
+++ src/br/com/alura/principal/PrincipalcomBusca.java
@@ -1,7 +1,17 @@
 package br.com.alura.principal;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class PrincipalcomBusca {
     public static void main(String[] args) {
-        
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://www.omdbapi.com/?i=tt3896198&apikey=43dbd051"))
+                .build();
+
+
     }
 }
