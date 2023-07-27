c112a616bfd1a666f76683f4bab88f8b5efd0f06
PedroAlmeida18 <pedrohas35@gmail.com>
2023-07-27 09:25:31 -0300

    Curso java orientação a objeto formação alura - AULA 01 busca de informações na api

--- src/br/com/alura/principal/PrincipalcomBusca.java
+++ src/br/com/alura/principal/PrincipalcomBusca.java
@@ -1,17 +1,22 @@
 package br.com.alura.principal;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class PrincipalcomBusca {
-    public static void main(String[] args) {
+    public static void main(String[]args)  throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create("http://www.omdbapi.com/?i=tt3896198&apikey=43dbd051"))
                 .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
 
 
     }
+
+
 }
