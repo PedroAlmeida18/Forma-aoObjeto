f2f5461bced8caa2f3dc36db118d4ded782a265c
PedroAlmeida18 <pedrohas35@gmail.com>
2023-08-08 09:14:38 -0300

    Curso java orientação a objeto formação alura - AULA 03: Tratando diversas exceções|Uso geral try e catch com conceitos ;

--- src/br/com/alura/principal/PrincipalcomBusca.java
+++ src/br/com/alura/principal/PrincipalcomBusca.java
@@ -18,27 +18,33 @@ public class PrincipalcomBusca {
         System.out.println("Digite o nome do Título que você deseja procurar:");
         var busca = leitura.nextLine();
         String end = "http://www.omdbapi.com/?t="+busca+ "&apikey=43dbd051";
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(end))
-                .build();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        String json = response.body();
-        System.out.println(json);
-        Gson gson = new GsonBuilder().
-                setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(end))
+                    .build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            String json = response.body();
+            System.out.println(json);
+            Gson gson = new GsonBuilder().
+                    setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
 
-        Tituloomdb meutituloomdb = gson.fromJson(json,Tituloomdb.class);
-        System.out.println(meutituloomdb);
-        try{
+            Tituloomdb meutituloomdb = gson.fromJson(json,Tituloomdb.class);
+            System.out.println(meutituloomdb);
+            //try{
             Titulo meuTitulo = new Titulo(meutituloomdb);
             System.out.println("titulo convertido ");
             System.out.println(meuTitulo);
         } catch (NumberFormatException e ){
             System.out.println("Aconteceu um erro");
             System.out.println(e.getMessage());
+        } catch (IllegalArgumentException a){
+            System.out.println("Aconteceu erro de busca na argumentação, verifique a busca");
+            System.out.println(a.getMessage());
         }
-        System.out.println("O PROGRAMA FINALIZOU CORRETAMENTE ");
+        System.out.println("O PROGRAMA FINALIZOU CORRETAMENTE "); // identaçao ctrl alt i
+        // error a máquina não consegue rodar
+        // Excepetion, posso prever
     }
 
 
